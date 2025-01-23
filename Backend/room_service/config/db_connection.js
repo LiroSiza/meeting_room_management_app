95c33db0aaed7ad9b4a01cbfeb22c7d50b13e3f9
CesarRT545 <cesarreyestorres005@gmail.com>
2025-01-23 14:40:11 -0600

    initial commit - Backend (services)

---
+++ Backend/room_service/config/db_connection.js
@@ -0,0 +1,19 @@
+/*
+    Conexion a la base de datos en la nube (mongoDB) utilizando variables de entorno
+    usando el archivo variables.env
+*/
+
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+const connectDB = async (params) => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB);
+        console.log("DB Connected - room service");
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
